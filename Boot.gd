9e9a080ce7d29653b17458570eb843b005facf30
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-04-10 14:56:11 +0200

    Removed old scripts Removed old scripts

--- Boot.gd
+++ Boot.gd
@@ -8,26 +8,26 @@ func _ready() -> void:
 #	if OS.has_feature("Server"):
 #		_ready_headless()
 #	else:
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	var arguments = {}
-	for argument in OS.get_cmdline_args():
-	# Parse valid command-line arguments into a dictionary
-		if argument.find("=") > -1:
-			var key_value = argument.split("=")
-			arguments[key_value[0].lstrip("--")] = key_value[1]
-	if arguments.has("server"):
-		if arguments.server == "true":
-			_ready_headless()
-	else:
-		MainMenu.show()	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+# 	var arguments = {}
+# 	for argument in OS.get_cmdline_args():
+# 	# Parse valid command-line arguments into a dictionary
+# 		if argument.find("=") > -1:
+# 			var key_value = argument.split("=")
+# 			arguments[key_value[0].lstrip("--")] = key_value[1]
+# 	if arguments.has("server"):
+# 		if arguments.server == "true":
+# 			_ready_headless()
+# 	else:
+# 		MainMenu.show()	
 
 
-func _ready_headless() -> void:
-	Log.hint(self, "_ready_headless", "Initializing Headless Server")
-	print("Starting server 1/4: Trying to load the world")
+# func _ready_headless() -> void:
+# 	Log.hint(self, "_ready_headless", "Initializing Headless Server")
+# 	print("Starting server 1/4: Trying to load the world")
 	
-	var player_data : Dictionary = {
-		name = "Server bot",
-		options = Options.player_data_set_pattern("server_bot")
-	}
-	Lobby.connect_to_server(player_data, true)
+# 	var player_data : Dictionary = {
+# 		name = "Server bot",
+# 		options = Options.player_data_set_pattern("server_bot")
+# 	}
+# 	Lobby.connect_to_server(player_data, true)
