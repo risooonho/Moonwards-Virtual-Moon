53fc089d147f1fa6fdc3823bda12d04e3c66245d
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-04-15 16:57:58 +0200

    Add command line args reading

---
+++ Script/Manager/CmdLineArgs.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var args: Dictionary = {} setget _set_illegal
+
+func _init():
+	for argument in OS.get_cmdline_args():
+		print(argument)
+		var arg_vals = argument.lstrip("--")
+		if argument.find("=") != -1:
+			arg_vals = arg_vals.split("=")
+			args[arg_vals[0]] = arg_vals[1]
+		else:
+			args[arg_vals] = true
+	#print(args)
+
+func _set_illegal(val) -> void:
+	Log.warnings(self, "_set_cmd", "Set invocation is illegal.")
