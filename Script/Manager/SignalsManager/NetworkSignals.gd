7088b6a34d4b54b4f4ddd48b511c4cf1edded628
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-04-23 16:52:53 +0200

    Refactor network signals + add them to menu

--- Script/Manager/SignalsManager/NetworkSignals.gd
+++ Script/Manager/SignalsManager/NetworkSignals.gd
@@ -1,23 +1,35 @@
 extends Reference
 class_name NetworkSignals
 
+### Until we or godot implements proper class_name handling
+const name = "Network"
+
+### LOBBY MANAGEMENT 
+
 # Fired when a request is received for a game to be started. 
 const GAME_SERVER_REQUESTED: String = "game_server_requested"
+
+signal game_server_requested
 
 # Fired when the process is initialized as a game server & is ready to receive connections
 const GAME_SERVER_READY: String = "game_server_ready"
 
+signal game_server_ready
+
 # Fired when the process is requested to be a client
 const GAME_CLIENT_REQUESTED: String = "game_client_requested"
+
+signal game_client_requested
 
 # Fired when the process is initiated as a client, is connected & is ready
 const GAME_CLIENT_READY: String = "game_client_ready"
 
-#warning-ignore:unused_signal
-signal game_server_requested
-#warning-ignore:unused_signal
-signal game_server_ready
-#warning-ignore:unused_signal
-signal game_client_requested
-#warning-ignore:unused_signal
 signal game_client_ready
+
+### LOADING & DATA MANAGEMENT
+
+# Fired when a connected player client has finished loading.
+# param `player_data`: The player data object
+const CLIENT_LOAD_FINISHED: String = "client_load_finished"
+
+signal client_load_finished(player_data)
--- Tree/Interface/MainMenu/MainMenu.gd
+++ Tree/Interface/MainMenu/MainMenu.gd
@@ -19,7 +19,7 @@ func hide() -> void:
 			i.visible = false
 
 func _on_bLocalGame_pressed() -> void:
-	tabs.current_tab = 3
+	Signals.Network.emit_signal(Signals.Network.GAME_SERVER_REQUESTED, true)
 
 func _on_bAbout_pressed() -> void:
 	tabs.current_tab = get_node( "Top/Tabs/About" ).get_position_in_parent()
@@ -34,3 +34,7 @@ func _on_bStart_pressed() -> void:
 #	var test_scene = preload("res://_tests/NewDemo/TestScene.tscn")
 #	get_tree().change_scene_to(test_scene)
 #	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _on_bJoinServer_pressed():
+	Signals.Network.emit_signal(Signals.Network.GAME_CLIENT_REQUESTED)
