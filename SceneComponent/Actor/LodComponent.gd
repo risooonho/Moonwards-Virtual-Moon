e3dd9224be5f642e906fe20493c231e101b8dac6
Karim Rizk <karim.essam.hamdy@gmail.com>
2020-05-20 21:25:40 +0200

    Update and optimize LOD
    * LOD now uses Areas instead of groups, making it much more efficient at detecting single changes instead of batch LOD updates.
    * Add logging to LOD
    * Remove LOD Manager
    * Add LodComponent, responsible for managing LODs instead, does so in relation to it's owning `Entity`'s position.
    * LOD now only has 2 ranges, `LOD0` and `LOD1`'s max distances.
    * Removed LOD based culling.
    * Removed redundant LOD test files
    * Removed Camera culling

--- SceneComponent/Actor/LodComponent.gd
+++ SceneComponent/Actor/LodComponent.gd
@@ -2,14 +2,44 @@ extends AComponent
 
 var last_pos: Vector3 = Vector3.ZERO
 
+export(float) var lod0_max_distance: float = 5
+export(float) var lod1_max_distance: float = 10
+
+onready var lod0: Area = $LOD0Area
+onready var lod1: Area = $LOD1Area
+
 func _init().("LodComponent", true):
 	pass
 
 func _ready():
+	lod0.get_node("CollisionShape").scale *= lod0_max_distance
+	lod1.get_node("CollisionShape").scale *= lod1_max_distance
+	
+	lod0.connect("area_entered", self, "_on_lod0_entered")
+	lod0.connect("area_exited", self, "_on_lod0_exited")
+	lod1.connect("area_entered", self, "_on_lod1_entered")
+	lod1.connect("area_exited", self, "_on_lod1_exited")
 	pass 
 
-func _physics_process(delta):
-	var pos = entity.global_transform.origin
-	if pos != last_pos:
-		last_pos = pos
-		Signals.Lod.emit_signal(Signals.Lod.LOD_POS_CHANGED, pos)
+### Perhaps clean up this code to be clear lod level setters instead
+func _on_lod0_entered(lod_node: Node) -> void:
+	if lod_node is LodModel:
+		Log.trace(self, "", "LodModel:%s entered lod0 range" %lod_node.name)
+		lod_node.set_lod(0)
+	
+func _on_lod0_exited(lod_node: Node) -> void:
+	if lod_node is LodModel:
+		Log.trace(self, "", "LodModel:%s exited lod0 range" %lod_node.name)
+		if lod1.overlaps_area(lod_node):
+			lod_node.set_lod(1)
+	
+func _on_lod1_entered(lod_node: Node) -> void:
+	if lod_node is LodModel:
+		Log.trace(self, "", "LodModel:%s entered lod1 range" %lod_node.name)
+		if !lod0.overlaps_area(lod_node):
+			lod_node.set_lod(1)
+	
+func _on_lod1_exited(lod_node: Node) -> void:
+	if lod_node is LodModel:
+		Log.trace(self, "", "LodModel:%s exited lod1 range" %lod_node.name)
+		lod_node.set_lod(2)
--- Script/ExtensionClass/LodModel.gd
+++ Script/ExtensionClass/LodModel.gd
@@ -1,28 +1,21 @@
-extends Spatial
+extends Area
 class_name LodModel
 
 var _lods = {}
 var lod_enabled: bool = false
-export(bool) var debug_lod = false
+export(bool) var debug_lod = true
 
 func _ready():
+	_generate_col_shape()
 	if self.has_node("LOD0") and self.has_node("LOD1") and self.has_node("LOD2"):
 		_lods[0] = $LOD0
 		_lods[1] = $LOD1
 		_lods[2] = $LOD2
 		lod_enabled = true
 		add_to_group(Groups.LOD_MODELS)
+		set_lod(2)
 	else:
-		lod_enabled = false		
-
-#func _ready():
-#	for i in range(0,3):
-#		var l = get_child(i)
-#		if l != null:
-#			_lods[i] = l
-#	if _lods.size() == 3:
-#		lod_enabled = true
-#	add_to_group(Groups.LOD_MODELS)
+		lod_enabled = false
 
 func set_lod(level: int)-> void:
 	if !lod_enabled:
@@ -30,10 +23,18 @@ func set_lod(level: int)-> void:
 	hide_all()
 	_lods[level].visible = true;
 	if debug_lod:
-		Log.trace(self, "", "Lod changed to %s for object %s" %[level, self])
+		Log.trace(self, "", "Lod changed to %s for object %s" %[level, self.name])
 
 func hide_all()-> void:
 	if !lod_enabled:
 		return
 	for i in range(_lods.size()):
 		_lods[i].visible = false;
+
+func _generate_col_shape():
+	var col = CollisionShape.new()
+	var shape = SphereShape.new()
+	shape.radius = 0.1
+	shape.margin = 0.1
+	col.shape = shape
+	self.add_child(col)
