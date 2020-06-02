3a62f2c53010d7cd68cd27fac3548eec2a4d0af1
Zach777 <MeteorCity3109@protonmail.com>
2020-06-02 13:12:52 -0400

    Add Player Interactor (#362)
    * Make Interactor's area radius proportional.
    
    * Give Player scene an Interactor.
    
    * Interactor works with the Player scene
    - Adjusted the vertical positioning of the Player's interactor.

--- SceneComponent/Actor/PlayerInteractor.gd
+++ SceneComponent/Actor/PlayerInteractor.gd
@@ -6,4 +6,8 @@ func _init().("PlayerInteractor", true) -> void :
 
 #Make Interactor have my Entity variable as it's user.
 func _ready() -> void :
-	$Interactor.user = entity
+	$Interactor.user = entity.get_path()
+
+func _unhandled_input(event : InputEvent) -> void :
+	if event.is_action_pressed("use") :
+		$Interactor.interact()
