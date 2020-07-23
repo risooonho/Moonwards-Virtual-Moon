9744c7027607b93a2df856bd2455791070294d3f
yael <yael.atletl@gmail.com>
2020-07-22 20:55:28 -0500

    State Editor now loads names recursively without errors

--- Tree/Tools/NPCEditor/States/States.gd
+++ Tree/Tools/NPCEditor/States/States.gd
@@ -1,22 +1,34 @@
-extends Node
+extends VSplitContainer
 
 var Behaviors : Array = []
 var Behavior_names : Array = []
 
 func _ready() -> void:
-	load_behaviors_in(OS.get_user_data_dir())
+	var dir = Directory.new()
+	dir.open(OS.get_user_data_dir())
+	dir.make_dir_recursive("behaviors")
+	load_behaviors_in(OS.get_user_data_dir()+"/behaviors")
 	#Loads User Defined Behaviors
-	load_behaviors_in("res://SceneComponent/Actor/Humanoid/NPCs/DefaultBehaviors/")
+	load_behaviors_in("res://SceneComponent/Actor/Humanoid/NPCs/DefaultBehaviors")
 	#Loads Default behaviors
+	list_behaviors()
 
+func list_behaviors():
+	for names in Behavior_names:
+		print(names)
+		$ViewMenuSplit/Container/ScrollContainer/Behaviors.add_item(names)
 
 func load_behaviors_in(path : String) -> void:
+	print(path)
 	var Dir = Directory.new()
 	if Dir.open(path) == OK:
 		Dir.list_dir_begin()
 		var file_name : String = Dir.get_next()
-		while file_name != "":
+		print(file_name)
+		while (file_name != ""):
+			print(file_name)
 			if not Dir.current_is_dir():
+				print("found file: ", file_name)
 				if file_name.ends_with(".jbt"):
 					var BT = ConfigFile.new()
 					BT.load(file_name)
@@ -24,8 +36,10 @@ func load_behaviors_in(path : String) -> void:
 					Behavior_names.append(file_name.trim_suffix(".jbt"))
 				else:
 					print_debug("Found a file that is not a BT")
-			else:
+			elif file_name.is_valid_filename() and file_name != "." and file_name != "..":
 				load_behaviors_in(file_name)
 			file_name = Dir.get_next()
+		Dir.list_dir_end()
 	else: 
 		print_debug("Error accessing ", path)
+		
