131bc7dae1ea0ffa2bd346af7ce75bbbaf8a3b37
yael <yael.atletl@gmail.com>
2020-08-01 21:59:57 -0500

    fixed functions in the extended NPC

--- SceneComponent/Actor/Humanoid/NPCs/MwNPC.gd
+++ SceneComponent/Actor/Humanoid/NPCs/MwNPC.gd
@@ -13,38 +13,38 @@ signal hear()
 func property_check(input, signals, variables):
 	#input is object, get the specific variable in the variable port and then
 	#passes it to the next node
-	var filter = get_variable_from_port(variables, 1)
+	var filter = _get_variable_from_port(variables, 1)
 	if input.has(filter):
-		emit_signal_from_port(input.get(filter), signals, 0)
+		_emit_signal_from_port(input.get(filter), signals, 0)
 
 func match(input, signals, variables):
 	#Checks if input matches with the variable input, if it does
 	#calls the next node
-	if input == get_variable_from_port(variables, 1):
-		emit_signal_from_port(true, signals, 0)
+	if input == _get_variable_from_port(variables, 1):
+		_emit_signal_from_port(true, signals, 0)
 
 func parallel_trigger(input, signals, variables):
 	#Takes the contents of a signal and distributes it along other two nodes
-	emit_signal_from_port(input, signals, 0)
-	emit_signal_from_port(input, signals, 1)
+	_emit_signal_from_port(input, signals, 0)
+	_emit_signal_from_port(input, signals, 1)
 
 func force_next_state(input, signals, variables):
 	emit_signal("next_state", true)
 
 func tri_v_decision(input, signals, variables):
-	var weight1 = get_variable_from_port(variables, 1)
-	var weight2 = get_variable_from_port(variables, 2)
-	var weigth3 = get_variable_from_port(variables, 3)
+	var weight1 = _get_variable_from_port(variables, 1)
+	var weight2 = _get_variable_from_port(variables, 2)
+	var weigth3 = _get_variable_from_port(variables, 3)
 	var maximum = max(weight1, max(weight2, weigth3))
 	if maximum == weight1:
-		emit_signal_from_port(input, signals, 1)
+		_emit_signal_from_port(input, signals, 1)
 	elif maximum == weight2:
-		emit_signal_from_port(input, signals, 2)
+		_emit_signal_from_port(input, signals, 2)
 	elif maximum == weigth3:
-		emit_signal_from_port(input, signals, 3)
+		_emit_signal_from_port(input, signals, 3)
 
 func play_global_sound(input, signals, variables):
-	var path = get_variable_from_port(variables, 0)
+	var path = _get_variable_from_port(variables, 0)
 	var sound_player = AudioStreamPlayer.new()
 	sound_player.stream = load(path)
 	sound_player.connect("finished", sound_player, "queue_free")
@@ -52,7 +52,7 @@ func play_global_sound(input, signals, variables):
 	sound_player.play()
 
 func play_3d_sound(input, signals, variables):
-	var path = get_variable_from_port(variables, 0)
+	var path = _get_variable_from_port(variables, 0)
 	var sound_player = AudioStreamPlayer3D.new()
 	sound_player.stream = load(path)
 	sound_player.connect("finished", sound_player, "queue_free")
