6c21bde63cad33ed630a954ba4b44063627fdada
Zach777 <MeteorCity3109@protonmail.com>
2020-08-03 22:16:09 -0400

    Add comments to AnimationController.gd

--- SceneComponent/Actor/Humanoid/AnimationController.gd
+++ SceneComponent/Actor/Humanoid/AnimationController.gd
@@ -1,5 +1,11 @@
 extends AComponent
 
+enum anim_state {
+	ON_GROUND = 0,
+	FLAILING = 1,
+	CLIMBING = 2
+}
+
 var walk_direction = Vector2.ONE
 var climb_direction = 1.0
 
@@ -49,25 +55,32 @@ func update_climb_animation(_delta):
 	var kb_pos = entity.global_transform.origin
 	var climb_progress
 	
+	#Determine where we should be setting our feet when climbing.
 	if entity.climb_point % 2 == 0:
 		climb_progress = abs((2.0 if climb_direction > 0.0 else 0.0) - abs((kb_pos.y - entity.stairs.climb_points[entity.climb_point].y) / entity.stairs.step_size))
 	else:
 		climb_progress = abs((2.0 if climb_direction > 0.0 else 0.0) - (1.0 + abs(kb_pos.y - entity.stairs.climb_points[entity.climb_point].y) / entity.stairs.step_size))
 	
+	#Which direction we are traveling in.
 	var forward_amount = entity.input.z
 	
+	#Determine which animation to play if we are at the top of the stairs or still climbing.
 	if entity.climb_point == entity.stairs.climb_points.size() - 1 and kb_pos.y > entity.stairs.climb_points[entity.climb_point].y and not forward_amount <= 0.0:
-		entity.animation_tree.set("parameters/State/current", 1)
+		entity.animation_tree.set("parameters/State/current", anim_state.FLAILING)
 	else:
-		entity.animation_tree.set("parameters/State/current", 2)
+		entity.animation_tree.set("parameters/State/current", anim_state.CLIMBING)
 	
+	#Change animation if the animation playing is wrong.
 	if forward_amount > 0.0:
 		if climb_direction == -1.0:
 			entity.animation_tree.set("parameters/ClimbDirection/current", 0)
 			climb_direction = 1.0
 	elif forward_amount < 0.0:
 		if climb_direction == 1.0:
-			climb_direction = -1.0
 			entity.animation_tree.set("parameters/ClimbDirection/current", 1)
+			climb_direction = -1.0
 	
 	entity.animation_tree.set("parameters/ClimbProgress/seek_position", climb_progress)
+
+
+
